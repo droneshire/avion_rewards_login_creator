705267dfefdfd55d69efde3bffacbe2159aeb695
Ross Yeager <ryeager12@gmail.com>
2023-08-06 16:50:14 -0700

    wip

--- src/account_creator.py
+++ src/account_creator.py
@@ -1,4 +1,5 @@
 import platform
+import time
 
 import undetected_chromedriver as uc
 from selenium import webdriver
@@ -9,9 +10,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from config import BASE_URL
 from util import log
-
-fake = faker.Faker()
 
 
 def get_driver() -> uc.Chrome:
@@ -44,121 +44,55 @@ class AccountCreator:
     def __init__(self):
         self.driver = get_driver()
 
-    def login(self, username, password):
+    def start_new_account(self) -> None:
         time.sleep(1)
-        driver.get("https://pl.imvu.com/next/home/")
-        driver.maximize_window()
+        self.driver.get(BASE_URL)
+        self.driver.maximize_window()
 
         wait = WebDriverWait(driver, 30)
         element = wait.until(
             EC.presence_of_element_located(
-                (By.XPATH, "/html/body/section/div/div/div/section/div/div/button")
+                (By.XPATH, '//*[@id="join-now"]/div[2]/div/fieldset/label[2]')
             )
         )
-        button = driver.find_element(
-            By.XPATH, "/html/body/section/div/div/div/section/div/div/button"
-        )  # cookie accept
-        button.click()
+        radio_button = self.driver.find_element(
+            By.XPATH, '//*[@id="join-now"]/div[2]/div/fieldset/label[2]'
+        )  # select No
+        radio_button.click()
 
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(self.driver, 10)
         element = wait.until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[7]/nav/ul[2]/button"))
+            EC.presence_of_element_located((By.XPATH, '//*[@id="join-now"]/div[2]/div/div[1]/a'))
         )
-        button = driver.find_element(By.XPATH, "/html/body/div[7]/nav/ul[2]/button")
-        button.click()
+        button = self.driver.find_element(By.XPATH, '//*[@id="join-now"]/div[2]/div/div[1]/a')
+        button.click()  # click Continue
 
-        input_field = driver.find_element(
-            By.XPATH, "/html/body/div[7]/section[2]/div/div/div/section/form/div[1]/input"
-        )
+    def input_email(self, email: str) -> None:
+        input_field = driver.find_element(By.NAME, "signup_displayname")
         input_field.send_keys(username)
 
-        input_field = driver.find_element(
-            By.XPATH, "/html/body/div[7]/section[2]/div/div/div/section/form/div[2]/div[2]/input"
-        )
+        input_field = driver.find_element(By.NAME, "signup_email")
+        # Send slow
+        email = "noddsgnso@outlook.com"
+        for character in email:
+            input_field.send_keys(character)
+            time.sleep(uniform(0.05, 0.1))
+
+        input_field = driver.find_element(By.ID, "password")
         input_field.send_keys(password)
 
-        button = driver.find_element(
-            By.XPATH, "/html/body/div[7]/section[2]/div/div/div/section/form/div[4]/button"
+        input_field = driver.find_element(By.ID, "confirm_password")
+        input_field.send_keys(password)
+
+        time.sleep(1)
+
+        input_field = driver.find_element(By.CLASS_NAME, "date-picker-input")
+        input_field.send_keys(generate_random_date_of_birth(wiek1, wiek2))
+
+        button = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="registration-submit"]'))
         )
         button.click()
 
-
-def create_account(name, wiek1, wiek2):
-    username = (
-        fake.user_name()
-        + random.choice("abcdefghijklmnopqrstuvwxyz")
-        + random.choice("abcdefghijklmnopqrstuvwxyz")
-    )
-    email = (
-        random.choice("abcdefghijklmnopqrstuvwxyz")
-        + random.choice("abcdefghijklmnopqrstuvwxyz")
-        + fake.email()
-    )
-    password = generate_random_string(10)
-    driver.get("https://pl.secure.imvu.com/welcome/ftux/account/")
-    driver.maximize_window()
-    WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "/html/body/section/div/div/div/section/div/div/button")
-        )
-    ).click()  # cookie accept
-
-    input_field = driver.find_element(By.NAME, "signup_displayname")
-    input_field.send_keys(username)
-
-    rndm = generate_random_string(10)
-
-    input_field = driver.find_element(By.NAME, "signup_email")
-    # Send slow
-    email = "noddsgnso@outlook.com"
-    for character in email:
-        input_field.send_keys(character)
-        time.sleep(uniform(0.05, 0.1))
-
-    input_field = driver.find_element(By.ID, "password")
-    input_field.send_keys(password)
-
-    input_field = driver.find_element(By.ID, "confirm_password")
-    input_field.send_keys(password)
-
-    time.sleep(1)
-
-    input_field = driver.find_element(By.CLASS_NAME, "date-picker-input")
-    input_field.send_keys(generate_random_date_of_birth(wiek1, wiek2))
-
-    button = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="registration-submit"]'))
-    )
-    button.click()
-    input("Press Enter to continue...")
-
-    print(username + ":" + password)
-    return f"{username}:{password}"
-
-
-def setup_account(name):
-    driver.get("https://pl.imvu.com/next/home/")
-
-    wait = WebDriverWait(driver, 10)
-    element = wait.until(EC.presence_of_element_located((By.ID, "saveAndExit")))
-    button = driver.find_element(By.ID, "saveAndExit")
-    button.click()
-
-    driver.get("https://pl.imvu.com/next/av/" + name)
-
-
-def main():
-    # Provide your 2captcha API key
-
-    create_account("xd144241", 19, 25)
-
-    time.sleep(10)
-
-    input("Press Enter to continue...")
-
-    # Close the browser
-    driver.quit()
-
-
-if __name__ == "__main__":
-    main()
+    def close(self) -> None:
+        self.driver.quit()

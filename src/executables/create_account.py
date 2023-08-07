88ded339d413f637305962bc95efbd88f8c7eead
Ross Yeager <ryeager12@gmail.com>
2023-08-06 22:26:52 -0700

    cleanup

--- src/executables/create_account.py
+++ src/executables/create_account.py
@@ -1,85 +1,19 @@
 import argparse
 import os
-import random
 import re
-import string
 
 import dotenv
 
 from account_creator import AccountCreator
 from config import PROJECT_NAME
 from email_parser import EmailParser
+from parse_args import parse_args
 from util import log, wait
-
-
-def generate_random_string(length: int) -> str:
-    # Define the characters you want to include in the random string
-    characters = string.ascii_letters + string.digits
-
-    # Generate the random string
-    random_string = "".join(random.choice(characters) for _ in range(length))
-
-    return random_string[0].upper() + random_string + "!"
 
 
 def is_valid_email(email: str) -> bool:
     regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
     return re.fullmatch(regex, email) is not None
-
-
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Property Guru Bot")
-
-    log_dir = log.get_logging_dir(PROJECT_NAME)
-
-    parser.add_argument("--log-dir", default=log_dir)
-    parser.add_argument(
-        "--log-level",
-        type=str,
-        help="Logging level",
-        default="INFO",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-    )
-    parser.add_argument(
-        "--dry-run",
-        action="store_true",
-        help="Run the script without actually sending SMS",
-    )
-
-    parser.add_argument(
-        "--email",
-        type=str,
-        help="Email address to login with",
-        default=os.environ.get("GMAIL_MAIN_EMAIL"),
-    )
-    parser.add_argument(
-        "--backup-email",
-        type=str,
-        help="Backup email address to register with",
-        default=os.environ.get("GMAIL_BACKUP_EMAIL"),
-    )
-    parser.add_argument(
-        "--password",
-        type=str,
-        help="Password to login all accounts with",
-        default=generate_random_string(10),
-    )
-    parser.add_argument(
-        "--manual-input",
-        action="store_true",
-        help="Manually input codes instead of email parser",
-    )
-    parser.add_argument(
-        "--close-on-exit",
-        action="store_true",
-        help="Close the browser on exit",
-    )
-    parser.add_argument(
-        "--save-after-read",
-        action="store_true",
-        help="Save email after reading instead of deleting",
-    )
-    return parser.parse_args()
 
 
 def get_login_code_from_parser(email: str, creds_env: str, delete_after_find: bool = False) -> str:
